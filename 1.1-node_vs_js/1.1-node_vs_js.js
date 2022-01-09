ef4a2be2b8cc225b97adac2b6b21ddd9e584e4e6
DanielShamir93 <daniel.shamir.93@gmail.com>
2022-01-09 14:26:07 +0200

    assignments 1.1 and 2.1 done

---
+++ 1.1-node_vs_js/1.1-node_vs_js.js
@@ -0,0 +1,5 @@
+// node runs on a server - not in a browser
+// the console is the terminal window
+// we use global object instead of window object
+// has common core modules
+// we use commonJS modules instead of ES6 modules
---
+++ 2.1-file_system/2.1-file_system.js
@@ -0,0 +1,37 @@
+const fsPromises = require("fs").promises;
+const fs = require("fs");
+const path = require("path");
+
+// 1
+// fs.writeFile(path.join(__dirname, "textFile.txt"), "\\ 1 done", (err) => {
+//   if (err) {
+//     throw err;
+//   }
+// });
+
+
+const fileOps = async () => {
+  try {
+    // 1
+    await fsPromises.writeFile(path.join(__dirname, "originTextFile.txt"), "Created new text file");
+    // 2
+    await fsPromises.copyFile(path.join(__dirname, "originTextFile.txt"), path.join(__dirname, "copyTextFile.txt"));
+    // 3
+    await fsPromises.rename(path.join(__dirname, "copyTextFile.txt"), path.join(__dirname, "copyAndRenameTextFile.txt"));
+    // 4
+    fs.readdir(__dirname, (err, files) => {
+      files.forEach(file => {
+        console.log(file);
+      });
+    });
+    // 5
+    await fsPromises.unlink(path.join(__dirname, "originTextFile.txt"));
+
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+fileOps();
+
+// 2
